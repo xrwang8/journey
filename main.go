d381128a7689975598c5a5afce167836c66a0f0b
xrwang8 <xrwang8@gmail.com>
2022-04-07 13:40:42 +0800

    新增 http demo

--- main.go
+++ main.go
@@ -2,15 +2,40 @@ package main
 
 import (
 	"fmt"
-	"log"
+	"io/ioutil"
 	"net/http"
 )
+
+type HelloServie interface {
+	SayHello() (string, error)
+}
+
+type Hello struct {
+}
+
+func (h Hello) SayHello() (string, error) {
+	client := http.Client{}
+	resp, err := client.Get("http://127.0.0.1:8080")
+	if err != nil {
+		return "", err
+	}
+	data, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+	return string(data), nil
+}
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello Go World!")
 }
 
 func main() {
-	http.HandleFunc("/", handler)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	helloService := Hello{}
+	resp, err := helloService.SayHello()
+	if err != nil {
+		fmt.Printf("err:%+v", err)
+		return
+	}
+	fmt.Printf("resp: %+v", resp)
 }
