aa4c9a72353f6a4c50a7c4ee356bf93f9524131a
xrwang8 <xrwang8@gmail.com>
2022-04-07 11:02:01 +0800

    新增 http demo

--- main.go
+++ main.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello Go World!")
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
