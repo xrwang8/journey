70bd6e05d32c5cbdbe99510ad5019bd4c5412fcf
xrwang8 <xrwang8@gmail.com>
2022-04-07 14:25:34 +0800

    反射demo

--- main.go
+++ main.go
@@ -4,16 +4,21 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"reflect"
 )
 
 type HelloServie interface {
-	SayHello() (string, error)
+	SayHello(name string) (string, error)
 }
 
 type Hello struct {
+	endpoint  string
+	FuncFiled func()
 }
 
-func (h Hello) SayHello() (string, error) {
+var _ HelloServie = Hello{}
+
+func (h Hello) SayHello(name string) (string, error) {
 	client := http.Client{}
 	resp, err := client.Get("http://127.0.0.1:8080")
 	if err != nil {
@@ -28,14 +33,45 @@ func (h Hello) SayHello() (string, error) {
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello Go World!")
+
+}
+
+// try reflect
+func PrintFuncName(val interface{}) {
+	t := reflect.TypeOf(val)
+
+	v := reflect.ValueOf(val)
+
+	//numMethod := t.NumMethod()
+	//for i := 0; i < numMethod; i++ {
+	//	methodName := t.Method(i)
+	//
+	//
+	//	fmt.Println(methodName.Name)
+	//
+	//}
+
+	field := t.NumField()
+	for i := 0; i < field; i++ {
+		fieldName := t.Field(i)
+		value := v.Field(i)
+		if value.CanSet() {
+			fmt.Println("fieldName:", fieldName.Name, "value:", value)
+		}
+
+	}
+
 }
 
 func main() {
-	helloService := Hello{}
-	resp, err := helloService.SayHello()
-	if err != nil {
-		fmt.Printf("err:%+v", err)
-		return
-	}
-	fmt.Printf("resp: %+v", resp)
+	// 没有构造函数，直接使用结构体
+	helloService := Hello{endpoint: "127.0.0.1:8080"}
+	//resp, err := helloService.SayHello("golang")
+	//if err != nil {
+	//	fmt.Printf("err:%+v", err)
+	//	return
+	//}
+	//fmt.Printf("resp: %+v", resp)
+
+	PrintFuncName(helloService)
 }
